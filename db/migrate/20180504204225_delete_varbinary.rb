7374dea60fd74fafde883adca58326d53cde3702
Rachel Bressner <rachel.bressner@gmail.com>
2018-05-04 17:41:37 -0400

    dddfdf

--- db/migrate/20180504204225_delete_varbinary.rb
+++ db/migrate/20180504204225_delete_varbinary.rb
@@ -1,8 +1,6 @@
 class DeleteVarbinary < ActiveRecord::Migration[5.2]
   def change
-    remove_column(:posts, :image)
     add_column(:posts, :image, :string)
-    remove_column(:users, :propic)
     add_column(:users, :propic, :string)
   end
 end
