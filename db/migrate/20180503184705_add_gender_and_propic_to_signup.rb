9e2d2c14dcaac4581e484e5bc56e3ae72a454f26
Rachel Bressner <rachel.bressner@gmail.com>
2018-05-04 17:39:11 -0400

    dddfdf

--- db/migrate/20180503184705_add_gender_and_propic_to_signup.rb
+++
@@ -1,6 +0,0 @@
-class AddGenderAndPropicToSignup < ActiveRecord::Migration[5.2]
-  def change
-    add_column(:users, :gender, :string)
-    add_column(:users, :propic, :varbinary)
-  end
-end
--- db/migrate/20180503204056_change_image_data_type_for_pg.rb
+++
@@ -1,8 +0,0 @@
-class ChangeImageDataTypeForPg < ActiveRecord::Migration[5.2]
-  def change
-    remove_column(:posts, :image)
-    add_column(:posts, :image, :bytea)
-    remove_column(:users, :propic)
-    add_column(:users, :propic, :bytea)
-  end
-end
