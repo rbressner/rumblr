98df7124c4f90d9b7e6029dac31ea937b64b15cb
Rachel Bressner <rachel.bressner@gmail.com>
2018-05-06 12:07:41 -0400

    some styling finally

--- models.rb
+++ models.rb
@@ -2,9 +2,6 @@ class User < ActiveRecord::Base
   has_one :profiles
   has_many :posts
   has_many :searches
-  def self.search(keyword)
-    where("username like ?", "%#{keyword}%")
-  end
 end
 
 
@@ -14,4 +11,7 @@ end
 
 class Search < ActiveRecord::Base
   belongs_to :users
+  def self.search(keyword)
+    where("username like ?", "%#{keyword}%")
+  end
 end
