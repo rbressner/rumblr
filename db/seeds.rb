d57ebf35fad556e15ee6628790934c6ab825485d
Rachel Bressner <rachel.bressner@gmail.com>
2018-06-11 13:09:52 -0400

    what's cooler than being cool?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 require "./models"
-User.create(username: "ray", lastname: "b", email: "ray@b.com", password: "123456", birthday: "1111-11-11", gender: "female")
+User.create(username: "ray", lastname: "b", email: "ray@b.com", password: "123456", birthday: "1111-11-11")
